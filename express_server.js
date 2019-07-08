4857c3c9e0d6c94cd7398324be1868d773b4fd45
Nelly31 <nelly_main@hotmail.com>
2019-07-08 21:47:51 +0000

    redirecting

--- express_server.js
+++ express_server.js
@@ -46,11 +46,22 @@ app.get("/urls/:shortURL", (req,res) => {
   res.render("urls_show",templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("OK");
+app.get("/u/:shortURL", (req,res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
+// app.post("/urls", (req, res) => {
+//   console.log(req.body);
+//   res.send("OK");
+// });
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
