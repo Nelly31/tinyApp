ad9ceda34a996cbf1daa858f125a18ec11180f24
Nelly31 <nelly_main@hotmail.com>
2019-07-12 02:00:07 +0000

    adding a link to the short URL in the index

--- resources.js
+++ resources.js
@@ -1,6 +1,11 @@
 const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "ba768n"},
-  "9sm5xK": {longURL: "http://www.google.com", userID: "biszb0"}
+  "9sm5xK": {longURL: "http://www.google.com", userID: "biszb0"},
+  "432jlk": {longURL: "https://kottke.org", userID: "biszb0"},
+  "65lkjq": {longURL: "https://www.wikipedia.org", userID: "ba768n"},
+  "54ssdj": {longURL: "https://www.bbc.com", userID: "biszb0"},
+  "fs65jl": {longURL: "https://example.com", userID: "ba768n"},
+  "8djasl": {longURL: "https://www.theguardian.com", userID: "ba768n"}
 };
 
 let users = {
